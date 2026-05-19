06e45bd7aba78bfa71f9c2452b8bbcb7d6760515
Anil Kambar <anilatsas01@gmail.com>
2026-05-19 15:29:38 +0530

    updated title

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import { TodoProvider } from "./contexts";
 import sasLogo from "./assets/sas-dark.png";
-import "./App.css";
+// import "./App.css";
 import TodoForm from "./components/TodoForm";
 import TodoItem from "./components/TodoItem";
 
